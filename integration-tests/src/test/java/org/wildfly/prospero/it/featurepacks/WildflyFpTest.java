4db5b66984c59bc8e6cfa4a40a026805cf65bbcf
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2023-02-28 14:31:17 +0000

    restoring test

---
+++ integration-tests/src/test/java/org/wildfly/prospero/it/featurepacks/WildflyFpTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2022 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wildfly.prospero.it.featurepacks;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.wildfly.prospero.cli.ReturnCodes;
+import org.wildfly.prospero.cli.commands.CliConstants;
+import org.wildfly.prospero.it.ExecutionUtils;
+import org.wildfly.prospero.test.MetadataTestUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+
+public class WildflyFpTest {
+
+    @Rule
+    public TemporaryFolder tempDir = new TemporaryFolder();
+
+    private Path targetDir;
+
+    @Before
+    public void setUp() throws IOException {
+        targetDir = tempDir.newFolder().toPath();
+    }
+
+    @Test
+    public void testInstallProsperoWithWildfly() throws Exception {
+        Path channelsFile = tempDir.newFile().toPath();
+        MetadataTestUtils.prepareChannel(channelsFile, "manifests/wildfly-27.0.1.Final-channel.yaml",
+                "manifests/prospero-manifest.yaml");
+
+        final URL provisionDefinition = this.getClass().getClassLoader().getResource("galleon/wfly-with-prospero.xml");
+
+        ExecutionUtils.prosperoExecution(CliConstants.Commands.INSTALL,
+                        CliConstants.CHANNELS, channelsFile.toString(),
+                        CliConstants.DEFINITION, Paths.get(provisionDefinition.toURI()).toString(),
+                        CliConstants.DIR, targetDir.toAbsolutePath().toString())
+                .withTimeLimit(20, TimeUnit.MINUTES)
+                .execute()
+                .assertReturnCode(ReturnCodes.SUCCESS_LOCAL_CHANGES);
+
+        final Path installedProspero = targetDir.resolve("bin").resolve(ExecutionUtils.isWindows()?"prospero.bat":"prospero.sh");
+        assertTrue(Files.exists(installedProspero));
+
+        // TODO: This doesn't work due to incompatible wildfly-channel library version. Re-enable after we get back to an
+        //  official wildfly-channel release.
+        ExecutionUtils.prosperoExecution(CliConstants.Commands.UPDATE, CliConstants.Commands.LIST,
+                CliConstants.DIR, targetDir.toAbsolutePath().toString())
+                .withTimeLimit(10, TimeUnit.MINUTES)
+                .execute(installedProspero)
+                .assertReturnCode(ReturnCodes.SUCCESS_NO_CHANGE);
+    }
+}
---
+++ integration-tests/src/test/java/org/wildfly/prospero/it/installationmanager/CreateSnapshotTest.java
@@ -0,0 +1,96 @@
+/*
+ *
+ *  * Copyright 2022 Red Hat, Inc. and/or its affiliates
+ *  * and other contributors as indicated by the @author tags.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *   http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.wildfly.prospero.it.installationmanager;
+
+import org.junit.Test;
+import org.wildfly.installationmanager.MavenOptions;
+import org.wildfly.installationmanager.spi.InstallationManager;
+import org.wildfly.prospero.api.InstallationMetadata;
+import org.wildfly.prospero.api.ProvisioningDefinition;
+import org.wildfly.prospero.cli.commands.CliConstants;
+import org.wildfly.prospero.it.commonapi.WfCoreTestBase;
+import org.wildfly.prospero.spi.ProsperoInstallationManager;
+import org.wildfly.prospero.spi.ProsperoInstallationManagerFactory;
+import org.wildfly.prospero.test.MetadataTestUtils;
+import org.wildfly.prospero.wfchannel.MavenSessionManager;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CreateSnapshotTest extends WfCoreTestBase {
+
+    @Test
+    public void createSnapshot() throws Exception {
+        // installCore
+        Path channelsFile = MetadataTestUtils.prepareChannel(CHANNEL_BASE_CORE_19);
+
+        final ProvisioningDefinition provisioningDefinition = defaultWfCoreDefinition()
+                .setChannelCoordinates(channelsFile.toString())
+                .build();
+        installation.provision(provisioningDefinition.toProvisioningConfig(),
+                provisioningDefinition.resolveChannels(CHANNELS_RESOLVER_FACTORY));
+
+        final InstallationManager manager = new ProsperoInstallationManagerFactory().create(outputPath, new MavenOptions(MavenSessionManager.LOCAL_MAVEN_REPO, false));
+
+        // generate snapshot
+        final Path snapshot = manager.createSnapshot(temp.newFolder().toPath());
+        assertTrue(Files.exists(snapshot));
+
+        // import snapshot
+        final InstallationMetadata installationMetadata = InstallationMetadata.fromMetadataBundle(snapshot);
+        assertEquals(1, installationMetadata.getProsperoConfig().getChannels().size());
+    }
+
+    @Test
+    public void generateApplyCommand() throws Exception {
+        final Path channelsFile = MetadataTestUtils.prepareChannel(CHANNEL_BASE_CORE_19);
+        final StringBuffer expected = new StringBuffer("prospero");
+        if (System.getProperty("os.name").toLowerCase(Locale.getDefault()).contains("windows")) {
+            expected.append(".bat");
+        } else {
+            expected.append(".sh");
+        }
+        expected.append(" ");
+        expected.append(CliConstants.Commands.UPDATE).append(" ").append(CliConstants.Commands.APPLY);
+        expected.append(" ");
+        expected.append(CliConstants.DIR).append("=\"").append(outputPath.toAbsolutePath()).append("\"");
+        expected.append(" ");
+        expected.append(CliConstants.UPDATE_DIR).append("=\"").append(Path.of("foo").toAbsolutePath()).append("\"");
+        expected.append(" ");
+        expected.append(CliConstants.YES);
+
+        final ProvisioningDefinition provisioningDefinition = defaultWfCoreDefinition()
+                .setChannelCoordinates(channelsFile.toString())
+                .build();
+
+        installation.provision(provisioningDefinition.toProvisioningConfig(),
+                provisioningDefinition.resolveChannels(CHANNELS_RESOLVER_FACTORY));
+
+
+        final ProsperoInstallationManager manager = (ProsperoInstallationManager) new ProsperoInstallationManagerFactory().create(outputPath, new MavenOptions(MavenSessionManager.LOCAL_MAVEN_REPO, false));
+        final String command = manager.generateApplyUpdateCommand(Paths.get("foo"));
+        assertEquals(expected.toString(), command);
+    }
+}
